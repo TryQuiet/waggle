0041178f3421b7ca3ffd7a4bca10d2d16748cfdc
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2021-03-22 12:54:33 +0100

    tor-stability

--- src/entryNode.ts
+++ src/entryNode.ts
@@ -2,6 +2,7 @@ import { Tor } from './torManager'
 import { DataServer } from './socket/DataServer'
 import { ConnectionsManager } from './libp2p/connectionsManager'
 import initListeners from './socket/listeners/'
+import {ZBAY_DIR_PATH} from './constants'
 import * as path from 'path'
 import * as os from 'os'
 import fs from 'fs'
@@ -9,16 +10,20 @@ import PeerId from 'peer-id'
 
 const main = async () => {
   const torPath = `${process.cwd()}/tor/tor`
-  const settingsPath = `${process.cwd()}/tor/torrc`
   const pathDevLib = path.join.apply(null, [process.cwd(), 'tor'])
+  if(!fs.existsSync(ZBAY_DIR_PATH)) {
+    fs.mkdirSync(ZBAY_DIR_PATH)
+  }
   const tor = new Tor({
     torPath,
-    settingsPath,
+    appDataPath: ZBAY_DIR_PATH,
+    controlPort: 9051,
     options: {
       env: {
         LD_LIBRARY_PATH: pathDevLib,
         HOME: os.homedir()
-      }
+      },
+      detached: true
     }
   })
   await tor.init()
@@ -26,18 +31,18 @@ const main = async () => {
   try {
     service1 = await tor.getServiceAddress(7788)
   } catch (e) {
-    service1 = await tor.addOnion({ virtPort: 7788, targetPort: 7788, privKey: process.env.HIDDEN_SERVICE_SECRET })
+    service1 = await tor.addNewService(7788,7788)
   }
   console.log('service1', service1)
 
   const dataServer = new DataServer()
   dataServer.listen()
-  const peerId = fs.readFileSync('entryNodePeerId.json')
+  const peerId = fs.readFileSync('peerId1.json')
   const parsedId = JSON.parse(peerId.toString()) as PeerId.JSONPeerId
   const peerIdRestored = await PeerId.createFromJSON(parsedId)
   const connectonsManager = new ConnectionsManager({
     port: 7788,
-    host: service1,
+    host: `${service1.onionAddress}.onion`,
     agentHost: 'localhost',
     agentPort: 9050
   })
