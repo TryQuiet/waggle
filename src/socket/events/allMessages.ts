f3a6a89f46419b9ef3a5c63366d0cbc51c48c08c
Emilia Marczyk <emilia.marczyk@rumblefish.pl>
2021-04-19 16:11:47 +0200

    Send all channel messages after they're loaded into memory

--- src/socket/events/allMessages.ts
+++ src/socket/events/allMessages.ts
@@ -1,6 +1,8 @@
+import { IMessage } from '../../storage/storage'
 import { EventTypesResponse } from '../constantsReponse'
 
-export const loadAllMessages = (socket, messages, channelAddress) => {
+export const loadAllMessages = (socket: any, messages: IMessage[], channelAddress: string) => {
+  console.log(`load ${messages.length} messages for ${channelAddress}`)
   socket.emit(EventTypesResponse.RESPONSE_FETCH_ALL_MESSAGES, {
     channelAddress,
     messages
