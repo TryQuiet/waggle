799a2882ca6326f1e6794a0d19a68e9f4ff9c07c
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2021-06-07 14:51:26 +0200

    changelog and linter

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Tor } from './torManager'
 import { DataServer } from './socket/DataServer'
 import { ConnectionsManager } from './libp2p/connectionsManager'
 import initListeners from './socket/listeners'
+// eslint-disable-next-line
 const version =  require('./../package.json').version
 
 export default {
