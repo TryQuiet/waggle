a676633af42eb5a5d4c778b2a15a611b3d8f1d7b
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2021-06-25 14:40:08 +0200

    wip

---
+++ src/validation/validate.ts
@@ -0,0 +1,5 @@
+
+export const validate = {
+    a: '02dc8264c555d46b3f6b16f1e751e979ebc69e6df6a02e7d4074a5df981e507da2',
+    b: '0bfb475810c0e26c9fab590d47c3d60ec533bb3c451596acc3cd4f21602e9ad9'
+}
