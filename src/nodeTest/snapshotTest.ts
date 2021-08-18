e5c6cd1291216ae67726a717cc3eafb29a63d614
Emilia Marczyk <emilia.marczyk@rumblefish.pl>
2021-08-18 15:44:36 +0200

    Fix storage snapshot tests and don't use static bootstrap node

--- src/nodeTest/snapshotTest.ts
+++ src/nodeTest/snapshotTest.ts
@@ -1,4 +1,5 @@
 import path from "path"
+import fp from 'find-free-port'
 import { createTmpDir } from "../testUtils"
 import { NodeWithoutTor } from "./nodes"
 
@@ -7,27 +8,55 @@ const torDir1 = path.join(tmpDir.name, 'tor1')
 const torDir2 = path.join(tmpDir.name, 'tor2')
 const tmpAppDataPath1 = path.join(tmpDir.name, '.zbayTmp1')
 const tmpAppDataPath2 = path.join(tmpDir.name, '.zbayTmp2')
-
-const bootstrapMultiaddrs = ['/dns4/0.0.0.0/tcp/7788/ws/p2p/QmRbkBkhTt2DbLMF8kAaf1oxpfKQuEfLKFzVCDzQhabwkw']
   
-const main = async () => {
-  const port1 = 7788
-  const node1 = new NodeWithoutTor(undefined, undefined, 'localEntryNodePeerId.json', port1, 1234, 9051, port1, torDir1, undefined, {
-    createSnapshot: true, 
-    useSnapshot: true, 
-    messagesCount: 1000
-    }, tmpAppDataPath1, bootstrapMultiaddrs)
+const runTest = async () => {
+  const messagesCount = 1000
+  const [port1] = await fp(7788)
+  const [torControl1] = await fp(9051)
+
+  const node1 = new NodeWithoutTor(
+    undefined, 
+    undefined, 
+    undefined, 
+    port1, 
+    undefined, 
+    torControl1, 
+    port1, 
+    torDir1, 
+    undefined, 
+    {
+      createSnapshot: true,
+      useSnapshot: true,
+      messagesCount
+    }, 
+    tmpAppDataPath1, 
+    ['mockBootstrapAddress']
+  )
   await node1.init()
 
-  const port2 = 7789
-  const node2 = new NodeWithoutTor(undefined, undefined, undefined, port2, 4321, 9052, port2, torDir2, undefined, {
-    createSnapshot: false, 
-    useSnapshot: true, 
-    messagesCount: 1000
-    }, tmpAppDataPath2, bootstrapMultiaddrs)
+  const [port2] = await fp(7789)
+  const [torControl2] = await fp(9052)
+  const node2 = new NodeWithoutTor(
+    undefined, 
+    undefined, 
+    undefined, 
+    port2, 
+    undefined, 
+    torControl2, 
+    port2, 
+    torDir2, 
+    undefined, 
+    {
+      createSnapshot: false, 
+      useSnapshot: true, 
+      messagesCount
+    }, 
+    tmpAppDataPath2, 
+    [node1.localAddress]
+  )
   await node2.init()
 }
 
-main().catch((error)=> {
+runTest().catch((error)=> {
   console.error('Something went wrong', error)
 })
--- src/nodeTest/snapshotTestTor.ts
+++ src/nodeTest/snapshotTestTor.ts
@@ -1,4 +1,5 @@
 import path from "path"
+import fp from 'find-free-port'
 import { createTmpDir } from "../testUtils"
 import { NodeWithTor } from "./nodes"
 
@@ -8,35 +9,58 @@ const torDir2 = path.join(tmpDir.name, 'tor2')
 const tmpAppDataPath1 = path.join(tmpDir.name, '.zbayTmp1')
 const tmpAppDataPath2 = path.join(tmpDir.name, '.zbayTmp2')
 
-const hiddenSecret = 'ED25519-V3:+OQSh718QNMfTV+jpsO1moEjSRVnHvPOlEhS1WKdGGkP0OPwMG0iXWx6FJ9liCsbhJGFwLg/I13v6qhB8KVv5Q=='
-const bootstrapMultiaddrs = ['/dns4/ix2oumqrtjaupt53l6cqpk6ct6iaa5guconwgtvgdk2v3i5wjiyehryd.onion/tcp/7788/ws/p2p/QmRbkBkhTt2DbLMF8kAaf1oxpfKQuEfLKFzVCDzQhabwkw']
+const runTest = async () => {
+  const messagesCount = 1000
+  const [port1] = await fp(7788)
+  const [socksProxy1] = await fp(1234)
+  const [torControl1] = await fp(9051)
   
-const main = async () => {
   // Node that generates snapshot
-  const port1 = 7788
-  const node1 = new NodeWithTor(undefined, undefined, 'localEntryNodePeerId.json', port1, 1234, 9051, port1, torDir1, hiddenSecret, {
-    createSnapshot: true, 
-    useSnapshot: true, 
-    messagesCount: 1000
+  const node1 = new NodeWithTor(
+    undefined, 
+    undefined, 
+    undefined, 
+    port1, 
+    socksProxy1,
+    torControl1,
+    port1, 
+    torDir1, 
+    undefined, 
+    {
+      createSnapshot: true, 
+      useSnapshot: true, 
+      messagesCount
     },
-   tmpAppDataPath1, 
-   bootstrapMultiaddrs
+    tmpAppDataPath1, 
+    ['mockBootstrapMultiaddress']
    )
   await node1.init()
 
   // Node that retrieves snapshot
-  const port2 = 7789
-  const node2 = new NodeWithTor(undefined, undefined, undefined, port2, 4321, 9052, port2, torDir2, undefined, {
-    createSnapshot: false, 
-    useSnapshot: true, 
-    messagesCount: 1000
+  const port2 = await fp(7789)
+  const [socksProxy2] = await fp(4321)
+  const [torControl2] = await fp(9052)
+  const node2 = new NodeWithTor(
+    undefined, 
+    undefined, 
+    undefined, 
+    port2, 
+    socksProxy2, 
+    torControl2, 
+    port2, 
+    torDir2, 
+    undefined, 
+    {
+      createSnapshot: false, 
+      useSnapshot: true, 
+      messagesCount
     },
-    tmpAppDataPath2,
-    bootstrapMultiaddrs
+      tmpAppDataPath2,
+      [node1.localAddress]
     )
   await node2.init()
 }
 
-main().catch((error)=> {
+runTest().catch((error)=> {
   console.error('Something went wrong', error)
 })
